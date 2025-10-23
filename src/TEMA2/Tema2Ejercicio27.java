da39f5c8b9bb8bcc37ae9c5656e57388eac4885d
Noah <nbermejoinsti@gmail.com>
2025-10-23 10:16:56 +0200

    .

--- src/TEMA2/Tema2Ejercicio27.java
+++ src/TEMA2/Tema2Ejercicio27.java
@@ -1,19 +1,26 @@
 package TEMA2;
 
-import java.util.Scanner;
-
 public class Tema2Ejercicio27 {
     public static void main(String[] args) {
-        int num = 2;
-        int contador = 0;
-//usar un contadorDePrimos que debe llegar hasta  que encuentre 20 primos
-        for (int i = 2; contador < 20; i++) {
 
-            if (num % i == 0){
-                contador++;
-                System.out.println(i);
+        int num = 2;
+        int contadorp = 0;
+
+//usar un contadorDePrimos que debe llegar hasta  que encuentre 20 primos
+        while (contadorp < 20) {
+            int contador = 0;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    contador++;
+                    break;
+                }
+            }
+            if (contador == 0) {
+                System.out.println(num);
+                contadorp++;
             }
             num++;
+
         }
     }
 }
--- src/TEMA2/Tema2Ejercicio28.java
+++ src/TEMA2/Tema2Ejercicio28.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Tema2Ejercicio28 {
     public static void main(String[] args) {
-        System.out.println("Dime un numero de 5 cifras");
+        System.out.println("Dime un numero");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
 
@@ -14,14 +14,16 @@ public class Tema2Ejercicio28 {
         System.out.println(randomInt);
 
         //cambia mirando bien el enunciado y quitando el if vacío
-        do {
-            if (num == randomInt){
-            }else {
-                System.out.println("has fallado");
-                System.out.println("Dime otro");
-                num = in.nextInt();
-            }
-        }while (num != randomInt);
-        System.out.println("has acertado");
+        for (int i = 0; i < 5; i++) {
+                if (num == randomInt) {
+                    System.out.println("has acertado");
+                    i=5;
+                } else {
+                    System.out.println("has fallado");
+                    System.out.println("Dime otro");
+                    num = in.nextInt();
+                }
+
+        }
     }
 }
