2a5376782163cfe92bdaccbdafe77d5df4d0a435
Noah <nbermejoinsti@gmail.com>
2025-11-12 14:00:28 +0100

    .

--- src/TEMA3/Tema3Ejercicio7.java
+++ src/TEMA3/Tema3Ejercicio7.java
@@ -5,20 +5,26 @@ import java.util.Scanner;
 public class Tema3Ejercicio7 {
     public static void main(String[] args) {
         int contador;
+        int num = 1;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese el numero y te digo si es primo o no: ");
-        int num = sc.nextInt();
-        contador = numero(num);
-        if (contador == 0){
-            System.out.println("es primo");
-        }else {
-            System.out.println("No es primo");
+        while (num != 0) {
+            System.out.print("Ingrese el numero y te digo si es primo o no: ");
+            num = sc.nextInt();
+            contador = numero(num);
+            if (contador == 0) {
+                System.out.println("es primo");
+            } else {
+                System.out.println("No es primo");
+            }
         }
     }
     public static int numero(int num) {
         int contador = 0;
+        if (num == 0) {
+            System.out.println("adios");
+        }
         if (num <= 1) {
-            System.out.println("no es primo");
+            contador++;
         } else {
             for (int i = 2; i < num -1 ; i++) {
                 if (num % i == 0){
