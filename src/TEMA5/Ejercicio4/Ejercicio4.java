a9af6683633207d9e6a91b93e8b12444b3d254f8
Noah <nbermejoinsti@gmail.com>
2026-02-09 08:33:51 +0100

    Update Ejercicio4.java

--- src/TEMA5/Ejercicio4/Ejercicio4.java
+++ src/TEMA5/Ejercicio4/Ejercicio4.java
@@ -12,7 +12,7 @@ public class Ejercicio4 {
         String producto;
         Set<String> compra = new HashSet<>();
 
-        while(opcion != 3) {
+        while(opcion != 5) {
             menu();
             opcion = sc.nextInt();
             switch (opcion) {
@@ -27,30 +27,26 @@ public class Ejercicio4 {
 
                     break;
                 case 2:
-                    System.out.println("Que producto quieres combrobar?");
-                    if (productos.contains(sc.next())) {
-                        System.out.println("El producto esta en la lista.");
-                    }else {
-                        System.out.println("El producto no esta en la lista.");
-                    }
+                    System.out.println(productos);
 
                     break;
                 case 3:
-                    System.out.println("adios");
-                    break;
-                case 4:
                     System.out.println("Que producto quieres añadir?");
                     producto = sc.next();
                     if (compra.contains(producto)) {
                         System.out.println("El producto ya esta en la lista.");
-                    }else {
+                    } else if (productos.contains(producto)) {
                         compra.add(producto);
+                    }else {
+                        System.out.println("El producto no esta en la lista.");
                     }
+                    productos.remove(producto);
                     break;
-                case  5:
-                    System.out.println(productos.retainAll(compra));
-                    System.out.println(productos);
-
+                case  4:
+                    System.out.println(compra);
+                    break;
+                case 5:
+                    System.out.println("adios");
                     break;
             }
         }
@@ -58,6 +54,6 @@ public class Ejercicio4 {
     public static void menu(){
         System.out.println("--- compra ---\n");
         System.out.println("Que quieres hacer?");
-        System.out.println("1 añadir producto a la lista \n" + "2 verificar producto de la lista \n" + "3 salir\n" + "4 añadir producto a la lista \n" + "5 verificar producto de la lista \n");
+        System.out.println("1 añadir producto a la lista \n" + "2 verificar producto de la lista \n" + "3 añadir producto al carrito \n" + "4 verificar producto del carrito\n" + "5 salir\n");
     }
 }
