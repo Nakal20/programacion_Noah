3a323b087a94afa1d85c5c7ee239f5c3046cfb3b
Noah <nbermejoinsti@gmail.com>
2026-02-18 13:08:11 +0100

    Update and rename ciclomotores.java to Ciclomotores.java

---
+++ src/TEMA4/POO3/Ejercicio1/Ciclomotores.java
@@ -0,0 +1,26 @@
+package TEMA4.POO3.Ejercicio1;
+
+public class Ciclomotores extends Vehiculo {
+    private int cilindrada;
+
+    private static final int CILINDRADA_DEFECTO = 75;
+
+    public Ciclomotores(String matricula, String modelo, String marca, String color, int cilindrada) {
+        super(matricula, modelo, marca, color);
+        this.cilindrada = cilindrada;
+    }
+
+    public Ciclomotores() {
+        this("1234bbb", "4x4", "kia", "rojo", CILINDRADA_DEFECTO);
+    }
+
+    public boolean carnet(int cilindrada){
+        boolean carnet;
+        if(cilindrada >= 120){
+            carnet = true;
+        }else {
+            carnet = false;
+        }
+        return carnet;
+    }
+}
