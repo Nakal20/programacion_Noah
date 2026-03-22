c9d5075aeca8a294c744ad6da9a3f7d76880febc
Noah <nbermejoinsti@gmail.com>
2026-03-22 22:11:48 +0100

    Add files via upload

---
+++ src/Tema7/B6.java
@@ -0,0 +1,51 @@
+package Tema7.ejerciciosB.B6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class B6 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        String numero = sc.nextLine();
+
+        try {
+            //buscamos el archivo para leer (poner ruta complenta, cambiar en clase a la correcta)
+            String pi = Files.readString(Paths.get("C:/Users/noah/IdeaProjects/Pruebas/src/Tema7/ejerciciosB/pi-million.txt"));
+
+            boolean encontrado = buscar(pi, numero);
+
+            if (encontrado) {
+                System.out.println("El número SÍ aparece en PI");
+            } else {
+                System.out.println("El número NO aparece en PI");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean buscar (String texto, String patron){
+
+        int n = texto.length();
+        int m = patron.length();
+
+        for (int i = 0; i <= n - m; i++) {
+
+            int j = 0;
+
+            // compara todos los caracteres
+            while (j < m && texto.charAt(i + j) == patron.charAt(j)) {
+                j++;
+            }
+
+            if (j == m) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
