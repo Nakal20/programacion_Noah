9e399957d28fea9f3173c7ff91e8e0ea47c778e5
Noah <nbermejoinsti@gmail.com>
2026-05-11 18:30:25 +0200

    Update MainPiloto.java

--- src/TEMA8/AADD/MainPiloto.java
+++ src/TEMA8/AADD/MainPiloto.java
@@ -1,5 +1,8 @@
 package TEMA8.AADD;
 
+import TEMA8.Asignatura;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class MainPiloto {
@@ -7,25 +10,38 @@ public class MainPiloto {
         Scanner sc = new Scanner(System.in);
 
         int opcion = 9;
+
+        int id;
+        String code;
+        String nombre;
+        String apellido;
+        String dob;
+        String nationality;
+        int constructorid;
         while (opcion != 0) {
             System.out.println("\n que quieres hacer?");
-            System.out.println("0. salir \n1. \n2. \n3. \n4. \n5. \n6. \n7.");
+            System.out.println("0. salir \n1.Añadir piloto \n2.Buscar piloto \n3.Mostrar todos los pilotos \n4.Actualizar piloto \n5.Borrar piloto \n6.Mostrar calificacion de pilotos \n7.Mostrar calificacion de equipos");
             opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1:
                     PilotosCRUD a = new PilotosCRUD();
                     System.out.println("id del piloto");
-                    int id = sc.nextInt();
+                    id = sc.nextInt();
                     System.out.println("codigo del piloto");
-                    String code = sc.next();
+                    code = sc.next();
                     System.out.println("nombre del piloto");
-                    String nombre = sc.next();
+                    nombre = sc.next();
                     sc.nextLine();
                     System.out.println("apellido del piloto");
-                    String apellido = sc.nextLine();
-                    System.out.println("");
-                    a.CreatePilot(id, nombre, apellido, esOblidatoria);
+                    apellido = sc.nextLine();
+                    System.out.println("fecha de nachimiento del piloto");
+                    dob = sc.next();
+                    System.out.println("nacionalidad del piloto");
+                    nationality = sc.next();
+                    System.out.println("id del constructor");
+                    constructorid = sc.nextInt();
+                    a.CreatePilot(id,code, nombre, apellido, dob, nationality, constructorid);
 
                     break;
                 case 2:
@@ -35,20 +51,50 @@ public class MainPiloto {
                     b.ReadPilot(driverid);
                     break;
                 case 3:
+                    PilotosCRUD c = new PilotosCRUD();
+                    List<Piloto2> listaPilotos = c.ReadPilots();
+
+                    for (int i = 0; i < listaPilotos.size(); i++) {
+                        System.out.println(listaPilotos.get(i));
+                    }
 
                     break;
                 case 4:
+                    PilotosCRUD d = new PilotosCRUD();
+                    System.out.println("id del piloto");
+                    id = sc.nextInt();
+                    System.out.println("codigo del piloto");
+                    code = sc.next();
+                    System.out.println("nombre del piloto");
+                    nombre = sc.next();
+                    sc.nextLine();
+                    System.out.println("apellido del piloto");
+                    apellido = sc.nextLine();
+                    System.out.println("fecha de nachimiento del piloto");
+                    dob = sc.next();
+                    System.out.println("nacionalidad del piloto");
+                    nationality = sc.next();
+                    System.out.println("id del constructor");
+                    constructorid = sc.nextInt();
+                    d.UpdatePilot(id,code, nombre, apellido, dob, nationality, constructorid);
                     break;
                 case 5:
+                    PilotosCRUD e = new PilotosCRUD();
+                    System.out.println("id del piloto");
+                    id = sc.nextInt();
+                    e.DeletePilot(id);
                     break;
                 case 6:
+                    PilotosCRUD f = new PilotosCRUD();
+                    f.ShowPilotClassification();
                     break;
                 case 7:
-                    PilotosCRUD h = new PilotosCRUD();
-                    h.ShowBuildersClassification();
+                    PilotosCRUD g = new PilotosCRUD();
+                    g.ShowBuildersClassification();
                     break;
 
             }
         }
+        System.out.println("Adios");
     }
 }
